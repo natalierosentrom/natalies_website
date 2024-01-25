ed25ac4a20d6fe99bc9b952793c97ebc15d8f2ba
natalierosentrom <122500064+natalierosentrom@users.noreply.github.com>
2024-01-25 10:41:00 -0800

    yay

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const sounds = ['bruh', 'vine-boom', 'dry-fart', 'metal-pipe-clang', 'pluh', 'yippee', 'Who-Invited-This-Kid', 'womp-womp',
 'spongebob-fog-horn', 'anime-wow','dun-dun-dun', 'elevator-song', 'sus', 'wet-fart', 'mr-krabs-walking','oouh-my-god',
-'punch', 'smosh-shut-up', 'y-r-u-gay', 'wrong', 'outro', ]
+'punch', 'smosh-shut-up', 'y-r-u-gay', 'wrong', 'outro', 'beatbox', 'damn son', 'SODA', 'bonk', 'airhorn', 'sexy wub', 
+'ba ba booey', 'fail', 'crickets', 'goofy ah', 'ohmagodwow', 'drama alert','this just in', 'alert', 'whip', 'whistle',
+'her arms were cut off', 'Root Beer No', 'Electric Zoo', 'and youre not really fine', 'danger alarm', 'sad violin', ]
 
 sounds.forEach(sound=> {
     const btn= document.createElement('button')
