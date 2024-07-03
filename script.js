57128734cd62a8d60691773a12200a291deb401c
natalierosentrom <122500064+natalierosentrom@users.noreply.github.com>
2024-07-03 16:25:15 -0700

    june favorite

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 const sounds = ['bruh', 'vine-boom', 'dry-fart', 'metal-pipe-clang', 'cymbal', 'goofy bonk', 'pluh', 'taco bell', 'yippee', 'Who-Invited-This-Kid', 'womp-womp',
-'spongebob-fog-horn', 'anime-wow','dun-dun-dun', 'elevator-song', 'sus', 'wet-fart', 'mr-krabs-walking','oouh-my-god',
-'punch', 'smosh-shut-up', 'y-r-u-gay', 'wrong', 'outro', 'beatbox', 'damn son', 'SODA', 'bonk', 'airhorn', 'sexy wub', 
-'ba ba booey', 'fail', 'crickets', 'goofy ah', 'ohmagodwow', 'drama alert','this just in', 'alert', 'whip', 'whistle',
-'her arms were cut off', 'Root Beer No', 'Electric Zoo', 'and youre not really fine', 'danger alarm', 'sad violin', 'bacon bacon bacon',
-'dun dun', 'what da hell', 'Star Trek Red Alert']
+    'spongebob-fog-horn', 'anime-wow', 'dun-dun-dun', 'elevator-song', 'sus', 'wet-fart', 'mr-krabs-walking', 'oouh-my-god',
+    'punch', 'smosh-shut-up', 'y-r-u-gay', 'wrong', 'outro', 'beatbox', 'damn son', 'SODA', 'bonk', 'airhorn', 'sexy wub',
+    'ba ba booey', 'fail', 'crickets', 'goofy ah', 'ohmagodwow', 'drama alert', 'this just in', 'alert', 'whip', 'whistle',
+    'her arms were cut off', 'Root Beer No', 'Electric Zoo', 'and youre not really fine', 'danger alarm', 'sad violin', 'bacon bacon bacon',
+    'dun dun', 'what da hell', 'Star Trek Red Alert']
 
-sounds.forEach(sound=> {
-    const btn= document.createElement('button')
+sounds.forEach(singleSoundFromList => createButton(singleSoundFromList))
+
+function createButton(soundName) {
+    const btn = document.createElement('button')
     btn.classList.add('btn')
 
-    btn.innerText = sound
+    btn.innerText = soundName
 
-    btn.addEventListener('click', ()=>{
+    btn.addEventListener('click', () => {
         stopSongs()
-        document.getElementById(sound).play()
+        document.getElementById(soundName).play()
     })
 
-    document.getElementById('buttons').
-    appendChild(btn)
+    const buttonsDiv = document.getElementById('buttons')
+    buttonsDiv.appendChild(btn)
+}
 
-})
+createButton("fun button")
 
-function stopSongs(){
+function stopSongs() {
     sounds.forEach(sound => {
-        const song = document.getElementById
-        (sound)
-        song.pause ()
+        const song = document.getElementById(sound)
+        song.pause()
         song.currentTime = 0;
-    } )
+    })
 }
